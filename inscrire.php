94a3bc1b15f332f4009a82d1fb1e768b8273eec6
kylian-duval <70692979+kylian-duval@users.noreply.github.com>
2020-12-24 14:22:09 +0100

    Update inscrire.php

--- inscrire.php
+++ inscrire.php
@@ -6,43 +6,47 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="css.css">
-    <?php include "fonction.php"; connectionbdd(); ?>
+    <?php include "fonction.php";
+    connectionbdd(); ?>
 </head>
+<?php if (isset($_SESSION['login'])) { ?>
 
-<body class="fun-color">
-    <div class="bienvenu">
-        <div class="centrer" id='myDIV'>
-            <form action="" method="POST">
-                <p>
-                    <span>Nom d'utilisateur :</span>
-                    <input type="text" name="LOGIN" placeholder="Nom d'utilisateur" required>
-                </p>
-                <p>
-                    <span>Crée votre mot de passe:</span>
-                    <input type="password" name="MDP" placeholder="Crée mots de passe" required>
-                </p>
-                <p>
-                    <span>Confirmer votre mot de passe :</span>
-                    <input type="password" name="CONFMDP" placeholder="confirmer votre mots de passe" required>
-                </p>
-                <p><input type="submit" value="S'inscrire"/> <INPUT TYPE="BUTTON" VALUE="Retour" onclick="window.location.href = 'index.php';"></p>
-            </form>
+    <body>
+        <?php menuco($BDD); ?>
+        <h4>vous etre deja iscrit vous pouvais pas vous ré instrire</h4>
+    </body>
+<?php } else { ?>
+
+    <body class="fun-color">
+        <div class="bienvenu">
+            <div class="centrer" id='myDIV'>
+                <form action="" method="POST">
+                    <p>
+                        <span>Nom d'utilisateur :</span>
+                        <input type="text" name="LOGIN" placeholder="Nom d'utilisateur" required>
+                    </p>
+                    <p>
+                        <span>Crée votre mot de passe:</span>
+                        <input type="password" name="MDP" placeholder="Crée mots de passe" required>
+                    </p>
+                    <p>
+                        <span>Confirmer votre mot de passe :</span>
+                        <input type="password" name="CONFMDP" placeholder="confirmer votre mots de passe" required>
+                    </p>
+                    <p><input type="submit" value="S'inscrire" /> <INPUT TYPE="BUTTON" VALUE="Retour" onclick="window.location.href = 'index.php';"></p>
+                </form>
+            </div>
         </div>
-    </div>
-</body>
-<?php 
+    </body>
+<?php
 
-    if(isset($_POST['MDP']) && isset($_POST['CONFMDP']))
-    {
-        if ($_POST['MDP'] != $_POST['CONFMDP'])
-        {
+    if (isset($_POST['MDP']) && isset($_POST['CONFMDP'])) {
+        if ($_POST['MDP'] != $_POST['CONFMDP']) {
             echo "les deux mots de passe ne correspondent pas";
-        }
-        else
-        {
+        } else {
             verifUser($BDD);
         }
     }
-    
-?>
+} ?>
+
 </html>
